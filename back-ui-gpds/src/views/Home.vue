8454d5bea98fe13515b522a5b8528d1a9ae8548e
eduardo-chan <105666521+eduardo-chan@users.noreply.github.com>
2024-12-03 23:52:07 -0600

    Update Home.vue

--- back-ui-gpds/src/views/Home.vue
+++ back-ui-gpds/src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>Home</h1>
-      <p>soy eduardooaaaaaOOO Bienvenido a la página de iniciooo.</p>
+      <p>soy eduardooaaaaaOOO Bienvenido a la página de inicioooo.</p>
     </div>
   </template>
   

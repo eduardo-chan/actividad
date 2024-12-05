b51cc8c0594c43c86bcde30d66d15120a590c48f
eduardo-chan <105666521+eduardo-chan@users.noreply.github.com>
2024-12-04 18:50:33 -0600

    Update About.vue

--- back-ui-gpds/src/views/About.vue
+++ back-ui-gpds/src/views/About.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>About</h1>
-      <p>Esta es la página de información actualización 4de dic 22s sadsd  s ss.</p>
+      <p>Esta es la página de información actualización 4de dic 22s sadsd     s ss.</p>
     </div>
   </template>
   
